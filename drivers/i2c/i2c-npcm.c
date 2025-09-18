ac997fdb7ab804e2426a9f6046ed54837d15b3d5
Stanley Chu <yschu@nuvoton.com>
2025-09-19 07:48:30 +0800

    i2c: npcm: Add deblock callback for bus recovery
    Add deblock callback to perform I2C bus recovery through the reset
    command. The recovery always resets the controller status, even if
    the bus is not stuck.
    
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/i2c/i2c-npcm.c
+++ drivers/i2c/i2c-npcm.c
@@ -213,7 +213,7 @@ static void npcm_i2c_reset(struct npcm_i2c_bus *bus)
 {
 	struct npcm_i2c_regs *reg = bus->reg;
 
-	debug("%s: module %d\n", __func__, bus->num);
+	printf("%s: module %d\n", __func__, bus->num);
 	/* disable & enable SMB moudle */
 	clrbits_8(&reg->ctl2, SMBCTL2_ENABLE);
 	setbits_8(&reg->ctl2, SMBCTL2_ENABLE);
@@ -248,7 +248,7 @@ static void npcm_i2c_recovery(struct npcm_i2c_bus *bus, u32 addr)
 	val = readb(&reg->ctl3);
 	/* Skip recovery, bus not stucked */
 	if ((val & SMBCTL3_SCL_LVL) && (val & SMBCTL3_SDA_LVL))
-		return;
+		goto reset;
 
 	printf("Performing I2C bus %d recovery...\n", bus->num);
 	/* SCL/SDA are not releaed, perform recovery */
@@ -279,6 +279,7 @@ static void npcm_i2c_recovery(struct npcm_i2c_bus *bus, u32 addr)
 	} else {
 		printf("Fail to recover I2C bus %d\n", bus->num);
 	}
+reset:
 	npcm_i2c_reset(bus);
 }
 
@@ -511,7 +512,7 @@ static int npcm_i2c_xfer(struct udevice *dev,
 	if (bus->started)
 		npcm_i2c_send_stop(bus, true);
 
-	if (err)
+	if (err && err != I2C_ERR_NACK)
 		npcm_i2c_recovery(bus, msg->addr);
 
 	return ret;
@@ -556,6 +557,13 @@ static int npcm_i2c_set_bus_speed(struct udevice *dev,
 	struct npcm_i2c_bus *bus = dev_get_priv(dev);
 
 	return npcm_i2c_init_clk(bus, speed);
+}
+
+static int npcm_i2c_deblock(struct udevice *dev)
+{
+	npcm_i2c_recovery(dev_get_priv(dev), 0);
+
+	return 0;
 }
 
 static int npcm_i2c_probe(struct udevice *dev)
@@ -614,6 +622,7 @@ static int npcm_i2c_probe(struct udevice *dev)
 static const struct dm_i2c_ops nuvoton_i2c_ops = {
 	.xfer		    = npcm_i2c_xfer,
 	.set_bus_speed	= npcm_i2c_set_bus_speed,
+	.deblock	= npcm_i2c_deblock,
 };
 
 static const struct udevice_id nuvoton_i2c_of_match[] = {
